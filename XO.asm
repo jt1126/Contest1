f167e837bbc39790500f4868f9ab899a7c4e510a
jt1126 <josephdigafe@gmail.com>
2025-11-13 13:03:33 -0500

    MESSAGES

--- XO.asm
+++ XO.asm
@@ -9,3 +9,21 @@ boardSize = ($ - board)
 currentPlayer BYTE 'X'     ; Current player (X or O)
 gameOver BYTE 0            ; 0 = game continues, 1 = game over
 winner BYTE 0              ; 'X', 'O', or 0 for draw
+
+; Messages
+msgTitle BYTE "TIC-TAC-TOE GAME",0
+msgInstructions BYTE "Players take turns. Enter 1-9 to place your mark.",0
+msgPlayerTurn BYTE "Player X's turn. Enter position (1-9): ",0
+msgPlayerX BYTE "Player X's turn. Enter position (1-9): ",0
+msgPlayerO BYTE "Player O's turn. Enter position (1-9): ",0
+msgInvalid BYTE "Invalid move! Try again.",0
+msgXWins BYTE "Player X wins!",0
+msgOWins BYTE "Player O wins!",0
+msgDraw BYTE "It's a draw!",0
+msgPlayAgain BYTE "Play again? (Y/N): ",0
+msgNewLine BYTE 0Dh, 0Ah, 0
+
+; Board display strings
+lineSeparator BYTE "---+---+---",0
+verticalLine BYTE " | ",0
+emptyCell BYTE "   ",0
