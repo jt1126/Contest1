ea8db5cbe9f2d9455a38e87369cb8c9a417d13c4
jt1126 <josephdigafe@gmail.com>
2025-11-15 15:35:53 -0500

    main

--- XO.asm
+++ XO.asm
@@ -27,3 +27,21 @@ msgNewLine BYTE 0Dh, 0Ah, 0
 lineSeparator BYTE "---+---+---",0
 verticalLine BYTE " | ",0
 emptyCell BYTE "   ",0
+
+.code
+
+; Main procedure
+main PROC
+    call Randomize          ; Initialize random number generator
+    
+GameLoop:
+    call InitializeGame
+    call DisplayBoard
+    call PlayGame
+    call DisplayResult
+    call AskPlayAgain
+    cmp eax, 0
+    jne GameLoop
+    
+    INVOKE ExitProcess, 0
+main ENDP
